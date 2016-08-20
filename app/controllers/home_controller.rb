ba8dc130bf7a901b4e6197f6007f01dc920ef901
Choi <317asdf@naver.com>
2016-08-20 14:05:16 +0900

    recommend

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
+require 'uri'
+require 'open-uri'
+
+
 class HomeController < ApplicationController
   def index
+  end
+  def recommend
+      
+      
+      if !params[:search]
+        @search = "%EA%B0%80%EB%B0%A9"
+      else
+        @search = params[:search].to_s
+        @search = URI.encode(@search)
+      end
+      doc = Nokogiri::HTML(open("http://shopping.naver.com/search/all_search.nhn?query="+@search+"&pagingIndex=1&pagingSize=40&productSet=total&viewType=list&sort=rel&frm=NVSHPAG&sps=N", "User-Agent" =>'ruby'))
+
+      @arr = Array.new
+      doc.css("._productLazyImg").each do |x|
+          @arr << x
+      end
+      
+      @name = Array.new
+      doc.css(".info > a").each do |x|
+          @name << x.inner_text
+      end
+      
+  end
+  def search
   end
 end
